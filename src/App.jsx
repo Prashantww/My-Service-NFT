abea924a7503ed42f07372a47ab421efb3d16070
Prashantww <prashant1warghude@gmail.com>
2025-11-15 21:11:24 +0530

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import ServiceCard from "./components/ServiceCard";
 import Agreement from "./components/Agreement";
 
 const App = () => {
+  // ----Service Card Info----
+
   const serviceData = [
     {
       imageUrl: "/serviceCards/Professional&Web3Services.png",
@@ -30,16 +32,22 @@ const App = () => {
       <div className="bgGradientBlob blob1"></div>
       <div className="bgGradientBlob blob2"></div>
       <div className="profileAndCountdown flex w-[86vw] justify-between">
+        {/* ----Profile Card (Desktop & Tab verison)---- */}
         <ProfileCard
           userImage={"/dummyProfile.png"}
           userName={"Emerson Philips"}
           units={3.2}
         ></ProfileCard>
+
+        {/* ----Countdown Timer (Desktop & Tab verison)---- */}
+
         <CountdownTimer
           timerValue={"02:10:52:37"}
           timerLable={"Ticket Sales Close In"}
         ></CountdownTimer>
       </div>
+
+      {/* ----Heading---- */}
       <h1>
         <span>Win 10 Years of</span> Exclusive Time & Service
       </h1>
@@ -48,6 +56,8 @@ const App = () => {
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco.
       </p>
+
+      {/* ----Hero Buttons---- */}
       <div className="flex gap-[24px] mt-[27px]">
         <HeroButton toLink="#">Connect Wallet</HeroButton>
         <HeroButton toLink="#" variant="primary-right">
@@ -59,7 +69,11 @@ const App = () => {
           Buy Tickets
         </HeroButton>
       </div>
+
+      {/* ----Clock Image---- */}
       <img className="clockImage" src="/clockImage.png" alt="" />
+
+      {/* ----Profile and Countdown (Mobile version)---- */}
       <div className="profileAndCountdownMobile">
         <ProfileCard
           userImage={"/dummyProfile.png"}
@@ -71,10 +85,14 @@ const App = () => {
           timerLable={"Ticket Sales Close In"}
         ></CountdownTimer>
       </div>
+
+      {/* ----Lottery Details Section---- */}
       <section className="lotteryDetails">
         <h2 className="mb-[12px]">Lottery Details</h2>
         <ProgressBar current={750} total={1000}></ProgressBar>
       </section>
+
+      {/* ----Service Section---- */}
       <section className="serviceProviderProfile">
         <h2>Service Provider Profile</h2>
 
